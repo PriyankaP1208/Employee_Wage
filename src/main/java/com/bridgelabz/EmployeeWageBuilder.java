ea34e905c8c74aa6d2036f3297d24c3109c4ff6b
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-19 20:10:04 +0530

    ADDED:Functionality to calculate part time wage

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,21 +1,24 @@
 package com.bridgelabz;
 
 public class EmployeeWageBuilder {
-    public static void main(String args[]){
-        int IS_PRESENT=1;
+    public static void main(String args[]) {
+        int IS_FULL_TIME=1;
+        int IS_PART_TIME=2;
         int WAGE_PER_HOUR=20;
-        int FullDayHour=8;
-        int dailyEmpWage;
-        double empCheck=Math.floor(Math.random()*10)%2;
-        if(empCheck==IS_PRESENT)
-        {
-            System.out.println("Employee is present");
-            dailyEmpWage=WAGE_PER_HOUR*FullDayHour;
-            System.out.println("Daily employee wage is:"+dailyEmpWage);
-        }
+        int empHrs=0;
+        int dailyEmpWage=0;
+        double partTimeWage=0;
+        double empCheck=Math.floor(Math.random()*10)%3;
+        if(empCheck==IS_PART_TIME)
+            empHrs=4;
+        else if(empCheck==IS_FULL_TIME)
+            empHrs=8;
         else
-        {
-            System.out.println("Employee is absent");
-        }
+            empHrs=0;
+        dailyEmpWage=empHrs*WAGE_PER_HOUR;
+        System.out.println("Daily employee wage:"+dailyEmpWage);
+        double partTimeHours=Math.floor(Math.random()*10)%8;
+        partTimeWage=(partTimeHours*WAGE_PER_HOUR);
+        System.out.println("Part time wage:"+partTimeWage);
     }
 }
