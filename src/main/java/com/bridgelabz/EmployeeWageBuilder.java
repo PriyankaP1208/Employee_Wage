0d09242d3dde35872ef2087b64da126da8393fe2
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-19 16:54:48 +0530

    ADDED:Functionality to calculate daily employee wage

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -3,10 +3,15 @@ package com.bridgelabz;
 public class EmployeeWageBuilder {
     public static void main(String args[]){
         int IS_PRESENT=1;
+        int WAGE_PER_HOUR=20;
+        int FullDayHour=8;
+        int dailyEmpWage;
         double empCheck=Math.floor(Math.random()*10)%2;
         if(empCheck==IS_PRESENT)
         {
             System.out.println("Employee is present");
+            dailyEmpWage=WAGE_PER_HOUR*FullDayHour;
+            System.out.println("Daily employee wage is:"+dailyEmpWage);
         }
         else
         {
